439dfec0385f20d0a64799f99fc4551c38b0aeb5
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2024-12-21 13:49:34 -0300

    [ FEATURE ] Importing font Roboto.

--- src/app/components/Button/components/ButtonOutline/index.tsx
+++ src/app/components/Button/components/ButtonOutline/index.tsx
@@ -1,7 +1,7 @@
 export const ButtonOutline = ({ ...props }) => {
   return (
     <button
-      className="border-2 border-primary-300 text-primary-300 text-lg font-bold py-2 rounded-full w-36 transition-all hover:bg-transparent hover:bg-primary-300 hover:text-neutral-full"
+      className="border-2 border-primary-300 text-primary-300 text-lg font-bold py-2 rounded-full w-36 transition-all hover:bg-primary-300 hover:text-neutral-full"
       {...props}
     ></button>
   );
